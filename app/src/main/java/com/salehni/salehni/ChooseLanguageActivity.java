ecbc9365948977e27d9140ce0717ec5577798f06
abuhusseinApps <abuhussein_ahmad@yahoo.com>
2020-07-20 09:38:45 +0300

    add sign in screen design

--- app/src/main/java/com/salehni/salehni/ChooseLanguageActivity.java
+++ app/src/main/java/com/salehni/salehni/ChooseLanguageActivity.java
@@ -1,14 +1,32 @@
 package com.salehni.salehni;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChooseLanguageActivity extends AppCompatActivity {
+
+    Button en_Btn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_language);
+
+        en_Btn = findViewById(R.id.en_Btn);
+
+        en_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLanguageActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
