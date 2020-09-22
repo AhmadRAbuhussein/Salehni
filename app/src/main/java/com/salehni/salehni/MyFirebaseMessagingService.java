eeda9fe838c0e15ab802b12d1f1b8b9af625b27b
abuhusseinApps <abuhussein_ahmad@yahoo.com>
2020-09-22 15:52:23 +0300

    notification with intent

--- app/src/main/java/com/salehni/salehni/MyFirebaseMessagingService.java
+++ app/src/main/java/com/salehni/salehni/MyFirebaseMessagingService.java
@@ -1,13 +1,14 @@
 package com.salehni.salehni;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
-
 import androidx.core.app.NotificationCompat;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.salehni.salehni.view.activities.SignUpActivity;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -15,7 +16,16 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+        String test_tag = remoteMessage.getData().get("test_tag");
+
+        Intent intent;
+        PendingIntent pendingIntent;
+        intent = new Intent(this, SignUpActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id").setContentIntent(pendingIntent)
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -28,5 +38,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
+
     }
 }
